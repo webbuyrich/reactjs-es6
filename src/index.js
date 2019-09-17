d325509ec06a46c69e17dc051351a8e75f348588
webbuyrich <webbuyrich@gmail.com>
2019-09-17 14:43:38 -0700

    added utils.js file and api const

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { constAndLet } from './examples';
+
+constAndLet()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
