b0227c6ccefebbd79cd1ae71a1b43be0067c6719
webbuyrich <webbuyrich@gmail.com>
2019-09-17 14:58:12 -0700

    Added styles file that exports submodules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Markup, Editor, Container, Column, Row, RuleInput, RuleLabel, StyleInput,
+Button, Document} from './styled'
 
 function App() {
   return (
-    <div>
-      <h2>Hello</h2>
-    </div>
+    <Container>
+      <Column>
+        <Button>
+          New Rule
+        </Button>
+      </Column>
+      <Column>
+        <Button>
+          Random Text
+        </Button>
+        
+        <Document>
+          <Editor />
+          <Markup />
+        </Document>        
+      </Column>
+    </Container>
   );
 }
 
